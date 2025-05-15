4f513678c9c495ae86325591f7a5122f3d17bff7
Mike Martin <mike@unsupported.me>
2025-05-15 14:12:31 -0500

    Updates template to use const config pattern and creates a window

---
+++ templates/zig_raylib/src/config.zig
@@ -0,0 +1,3 @@
+pub const screen_width = 1280;
+pub const screen_height = 800;
+pub const screen_title = "CHANGE ME";
--- templates/zig_raylib/src/main.zig
+++ templates/zig_raylib/src/main.zig
@@ -1,6 +1,18 @@
 const std = @import("std");
+const rl = @import("raylib");
+const cfg = @import("config.zig");
 
 pub fn main() !void {
+    rl.initWindow(cfg.screen_width, cfg.screen_height, cfg.screen_title);
+    defer rl.closeWindow();
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.white);
+        rl.drawText("Hello raylib world", 0, 0, 32, rl.Color.light_gray);
+    }
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 }
