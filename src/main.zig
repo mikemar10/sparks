df756e6f93272e6035da685fe0ded06547c71cea
Mike Martin <mike@unsupported.me>
2025-05-14 20:35:42 -0500

    sets up raylib, opens a window, establishes main db struct

---
+++ src/main.zig
@@ -0,0 +1,67 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+
+const std = @import("std");
+const rl = @import("raylib");
+
+const State = enum {
+    start,
+    wait,
+    foul,
+    click,
+    results,
+};
+
+const DB = struct {
+    state: State = State.start,
+    delay_duration: u64 = 0,
+    reflex_duration: u64 = 0,
+    timer_start: std.time.Instant,
+
+    const Self = @This();
+    pub fn reset(self: Self) void {
+        self = Self{
+            .state = State.start,
+            .delay_duration = 0,
+            .reflex_duration = std.crypto.Random.intRangeAtMost(u64, 500, 2000),
+            .timer_start = std.time.Instant.now(),
+        };
+    }
+};
+
+const start_color = rl.Color.blue;
+const wait_color = rl.Color.blue;
+const foul_color = rl.Color.red;
+const click_color = rl.Color.green;
+const results_color = rl.Color.blue;
+
+pub fn main() !void {
+    const screenWidth = 1280;
+    const screenHeight = 800;
+    rl.initWindow(screenWidth, screenHeight, "reflex test");
+    defer rl.closeWindow();
+
+    //rl.setTargetFPS(60);
+
+    while (!rl.windowShouldClose()) {
+        rl.beginDrawing();
+        defer rl.endDrawing();
+
+        rl.clearBackground(rl.Color.black);
+        rl.drawText("Congrats! You finally got around to doing this", 190, 200, 20, rl.Color.light_gray);
+    }
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+
+    // stdout is for the actual output of your application, for example if you
+    // are implementing gzip, then only the compressed bytes should be sent to
+    // stdout, not any debugging messages.
+    const stdout_file = std.io.getStdOut().writer();
+    var bw = std.io.bufferedWriter(stdout_file);
+    const stdout = bw.writer();
+
+    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+
+    try bw.flush(); // Don't forget to flush!
+}
