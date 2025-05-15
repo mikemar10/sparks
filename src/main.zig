900563289d448a7ece91120f08dcc4da16eef55d
Mike Martin <mike@unsupported.me>
2025-05-14 21:39:30 -0500

    draws FPS

--- src/main.zig
+++ src/main.zig
@@ -65,6 +65,8 @@ pub fn main() !void {
         rl.beginDrawing();
         defer rl.endDrawing();
 
+        rl.drawFPS(0, 32);
+
         switch (db.state) {
             .start => {
                 rl.clearBackground(start_color);
@@ -120,17 +122,4 @@ pub fn main() !void {
             },
         }
     }
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
 }
